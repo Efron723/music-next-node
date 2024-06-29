d10ac932743ec6c8fb7e400faedf43a7a7f3003a
Efron723 <a161026521@gmail.com>
2024-06-29 18:35:45 +0800

    desktop-concert-third-complete

--- components/layout/default-layout/index.js
+++ components/layout/default-layout/index.js
@@ -12,13 +12,11 @@ export default function DefaultLayout({ title = 'Music', children }) {
       <div className="d-flex flex-column vh-100">
         <Nav />
         {/* main 裡面放背景顏色或背景 */}
-        <main className="flex-grow-1 bg-purple1">
+        <main className="flex-grow-1 bg-dark">
           {/* 使用自定義 container */}
-          <div className="music-container">
-            {children}
-            {/* 全域的載入動畫指示器 */}
-            {/* {loader()} */}
-          </div>
+          {children}
+          {/* 全域的載入動畫指示器 */}
+          {/* {loader()} */}
         </main>
         <Footer />
       </div>
--- components/layout/default-layout/nav.js
+++ components/layout/default-layout/nav.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import styles from './default.module.css'
+import { BsPersonCircle, BsBell, BsCart } from 'react-icons/bs'
+import LoginModal from '@/components/login/login-modal'
 
 export default function Nav() {
+  const [wakeModal, setWakeModal] = useState(false)
+
+  const handleModal = () => {}
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-purple3">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        aria-label="Eighth navbar example"
+        style={{ borderBottom: '1px solid var(--Primary-03, #DBD7FF)' }}
+      >
+        <div className="container">
+          {/* Logo */}
+          <Link className="navbar-brand" href="/">
+            Logoo
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -17,68 +30,120 @@ export default function Nav() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" />
           </button>
+          {/* 分頁連結 */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+                <Link
+                  className="nav-link active eng-p"
+                  aria-current="page"
+                  href="/activity"
+                >
+                  演出活動
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+                <Link className="nav-link eng-p" href="/product">
+                  周邊商城
+                </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
+              <li className="nav-item">
+                <Link className="nav-link eng-p" href="/artist">
+                  音樂人
+                </Link>
+              </li>
+            </ul>
+            {/* 右方icon */}
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 icon-lg-size">
+              <li className={`${styles['icon-margin']}`}>
+                <Link href="#" className="nav-link">
+                  <BsCart />
+                </Link>
+              </li>
+              <li className="icon-margin dropdown">
+                <Link
+                  className="nav-link"
                   href="#"
+                  id="navbarDropdown01"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
+                  <BsBell />
+                </Link>
+                <ul
+                  className="dropdown-menu dropdown-menu-dark dropdown-menu-end"
+                  aria-labelledby="navbarDropdown01"
+                >
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
+                    <Link className="dropdown-item" href="#">
+                      通知1
+                    </Link>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
+                    <Link className="dropdown-item" href="#">
+                      通知2
+                    </Link>
                   </li>
                 </ul>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
+              <li className="icon-margin dropdown">
+                <Link
+                  className="nav-link"
+                  href="#"
+                  id="navbarDropdown02"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <BsPersonCircle />
+                </Link>
+                <ul
+                  className="dropdown-menu dropdown-menu-dark dropdown-menu-end"
+                  aria-labelledby="navbarDropdown02"
+                >
+                  <div className={`${styles['mouse-cursor']}`}>
+                    <div
+                      className="dropdown-item"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      登入
+                    </div>
+                  </div>
+                  <li>
+                    <Link
+                      className="dropdown-item"
+                      href="/login/login-modal"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      註冊
+                    </Link>
+                    {/* <button
+                      className="btn btn-primary"
+                      onClick={handleRegisterClick}
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      會員中心
+                    </button>
+                    <LoginModal
+                      isActive={isActive}
+                      handleRegisterClick={handleRegisterClick}
+                      handleLoginClick={handleLoginClick}
+                    /> */}
+                  </li>
+                  <li>
+                    {/* <Link className="dropdown-item" href="#">
+                      登出
+                    </Link> */}
+                  </li>
+                </ul>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
